182b8cfeafd9c216546eec257c2d18d245d95a5e
unknown <azeemolatunji@gmail.com>
2022-04-20 13:59:50 +0100

    method for new user

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -7,4 +7,14 @@ class ExpendituresController < ApplicationController
     @expenditures = @category.expenditures.order(created_at: :desc)
     @total_amount = @expenditures.sum(:amount)
   end
+
+  # GET /expenditures/1 or /expenditures/1.json
+  def show; end
+
+  # GET /expenditures/new
+  def new
+    @user = current_user
+    @category = Category.find(params[:category_id])
+    @expenditure = Expenditure.new
+  end
 end
