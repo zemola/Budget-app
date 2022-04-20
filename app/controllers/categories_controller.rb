368b8320ed2f21d585a6f1e5cb73f17522bf4a10
unknown <azeemolatunji@gmail.com>
2022-04-20 13:52:18 +0100

    update categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -30,19 +30,6 @@ class CategoriesController < ApplicationController
     redirect_to root_path
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @category.update(category_params)
-  #       format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @category }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @category.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   # DELETE /categories/1 or /categories/1.json
   def destroy
     @category = Category.find(params[:id])
