0701e07a46b292b780af2741ab1cbf82b911a0ce
unknown <azeemolatunji@gmail.com>
2022-04-20 13:58:40 +0100

    method for getting expenditure

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,2 +1,10 @@
 class ExpendituresController < ApplicationController
+  before_action :set_expenditure, only: %i[show edit update destroy]
+
+  # GET /expenditures or /expenditures.json
+  def index
+    @category = Category.find(params[:category_id])
+    @expenditures = @category.expenditures.order(created_at: :desc)
+    @total_amount = @expenditures.sum(:amount)
+  end
 end
